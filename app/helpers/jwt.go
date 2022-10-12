39d32ec13176f0128465779bb1ad8de141ceb2cc
joaovicdsantos <joaovicdsantos@gmail.com>
2022-10-12 16:53:53 -0300

    refactor: improve jwt helpers

--- app/helpers/jwt.go
+++ app/helpers/jwt.go
@@ -26,27 +26,26 @@ func CreateJwtToken(user models.User) (string, error) {
 		return "", err
 	}
 
-	return fmt.Sprintf("%s", tokenString), nil
+	return tokenString, nil
 }
 
 func ParseJwtToken(unparsedToken string) (map[string]interface{}, error) {
 
 	splitedToken := strings.Split(unparsedToken, " ")
-	if splitedToken[0] != prefix {
-		return nil, fmt.Errorf("Invalid token")
+	if splitedToken[0] != prefix || len(splitedToken) != 2 {
+		return nil, fmt.Errorf("invalid token")
 	}
 
 	unparsedToken = splitedToken[1]
 	token, err := jwt.Parse(unparsedToken, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 		return []byte(jwtSecret), nil
 	})
 
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 		return claims, nil
-	} else {
-		return nil, err
 	}
+	return nil, err
 }
